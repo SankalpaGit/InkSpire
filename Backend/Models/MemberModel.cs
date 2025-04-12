9d5292109effd90df499957cbacd17b199290c90
Sankalpa <joshisankalpa2@gmail.com>
2025-04-12 07:07:17 +0545

    empty project initialized

---
+++ Backend/Models/MemberModel.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models;
+
+public class MemberModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; }
+
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
