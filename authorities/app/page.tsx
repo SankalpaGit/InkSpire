bdd8ab64bd275bd85135067cd765869ef17bb4f4
Sankalpa <joshisankalpa2@gmail.com>
2025-04-21 08:53:59 +0545

    Add initial project setup with Tailwind CSS, React components, and Vite configuration

---
+++ authorities/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-4xl font-bold">Authorities</h1>
+      <p className="mt-4 text-lg">This is the Authorities page.</p>
+      
+    </main>
+  );
+}
